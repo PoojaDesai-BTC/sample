d71b68089a90bd2fd1d6a40aed59c5a571e5a75b
Pooja <pooja.desai@beaverlogic.com>
2014-07-01 15:09:06 +0530

    Finished sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should be_valid }
 
@@ -76,6 +77,11 @@ describe "when password is not present" do
     describe "with valid password" do
       it { should eq found_user.authenticate(@user.password) }
     end
+
+    describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
